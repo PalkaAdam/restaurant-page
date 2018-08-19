78be2649c48e27ed06612afe78cd4822b2b9c8ed
PalkaAdam <pauka.adam@gmail.com>
2018-08-19 11:31:25 +0200

    Expand templates

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
 
-def index (request):
+def index(request):
     return render(request, 'index.html')
-# Create your views here.
+
+
+def menu(request):
+    return render(request, 'menu.html')
+
+
+def base(request):
+    return render(request, 'base.html')
